cbdf8ef1da522be218aeee935c89ca1276347fc7
Saketh Ayyagari <sakethsarma07@gmail.com>
2024-07-10 10:33:13 -0400

    assisted driving code

---
+++ assisted_driving.py
@@ -0,0 +1,106 @@
+"""
+MIT BWSI Autonomous RACECAR
+MIT License
+racecar-neo-prereq-labs
+
+File Name: lab1.py
+
+Title: Lab 1 - RACECAR Controller
+
+Author: Team 8
+
+Purpose: Using a Python script and the data polled from the controller module,
+write code to replicate a manual control scheme for the RACECAR. Gain a mastery
+in using conditional statements, controller functions and an understanding in the
+rc.drive.set_speed_angle() function. Complete the lines of code under the #TODO indicators 
+to complete the lab.
+
+Expected Outcome: When the user runs the script, they are able to control the RACECAR
+using the following keys:
+- When the right trigger is pressed, the RACECAR drives forward
+- When the left trigger is pressed, the RACECAR drives backward
+- When the left joystick's x-axis has a value of greater than 0, the RACECAR's wheels turns to the right
+- When the left joystick's x-axis has a value of less than 0, the RACECAR's wheels turns to the left
+- When the "A" button is pressed, increase the speed and print the current speed to the terminal window
+- When the "B" button is pressed, reduce the speed and print the current speed to the terminal window
+- When the "X" button is pressed, increase the turning angle and print the current turning angle to the terminal window
+- When the "Y" button is pressed, reduce the turning angle and print the current turning angle to the terminal window
+
+Environment: Test your code using the level "Neo Labs > Lab 1: RACECAR Controller".
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+
+# If this file is nested inside a folder in the labs folder, the relative path should
+# be [1, ../../library] instead.
+sys.path.insert(1, '../../library')
+import racecar_core
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+rc = racecar_core.create_racecar()
+
+# Declare any global variables here
+global speed
+global angle
+global speed_offset
+global angle_offset
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+# [FUNCTION] The start function is run once every time the start button is pressed
+def start():
+    global speed
+    global angle
+    global speed_offset
+    global angle_offset
+
+    speed = 0.0 # The initial speed is at 1.0
+    angle = 0.0 # The initial turning angle is 0.0
+    speed_offset = 0.05 # The initial speed offset is 0.5
+    angle_offset = 0.05 # The inital angle offset is 0.5
+
+    # This tells the car to begin at a standstill
+    rc.drive.stop()
+
+# [FUNCTION] After start() is run, this function is run once every frame (ideally at
+# 60 frames per second or slower depending on processing speed) until the back button
+# is pressed  
+def update():
+    global speed
+    global angle
+    global speed_offset
+    global angle_offset
+    
+    
+      
+    
+    (x, y) = rc.controller.get_joystick(rc.controller.Joystick.LEFT)
+    if x > 0:
+        angle = angle_offset
+    elif x < 0:
+        angle = -angle_offset
+    else:
+        angle = 0
+
+
+    # Send the speed and angle values to the RACECAR
+    rc.drive.set_speed_angle(speed, angle)
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update)
+    rc.go()
